1a841cd05309f391d9c90f746679e9009f1373d4
fuadop <fuadolatunji@gmail.com>
2024-06-21 07:36:16 +0100

    add diagnostic list user commands

--- lua/internal/commands/lsp_commands.lua
+++ lua/internal/commands/lsp_commands.lua
@@ -9,4 +9,30 @@ return {
 			desc = 'Format current buffer with LSP',
 		},
 	},
+
+	-- :Dl - Diagnostic list [in quickfix], all severity
+	{
+		'Dl',
+		function()
+			vim.diagnostic.setqflist()
+		end,
+		{
+			desc = 'Find diagnostic messages of current buffer and put in the quickfix list'
+		},
+	},
+
+	-- :De - Diagnostic list [in quickfix], error severity
+	{
+		'De',
+		function()
+			vim.diagnostic.setqflist(
+				{
+					severity = 1,
+				}
+			)
+		end,
+		{
+			desc = 'Find diagnostic messages of current buffer and put in the quickfix list'
+		},
+	}
 }
