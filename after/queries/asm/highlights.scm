de4fb9cb4e396a1cd4ad78e49239ddef5b28a91f
fuadop <fuad@nodelayer.xyz>
2024-08-11 23:05:54 +0100

    :bug: fix(highlights): asm comment node name

--- after/queries/asm/highlights.scm
+++ after/queries/asm/highlights.scm
@@ -1,6 +1,13 @@
 ;; extends
 (
-	(comment) @comment.todo
+	(line_comment) @comment.todo
+	(#match? @comment.todo "(todo|TODO|Todo)")
+	(#set! "priority" 150)
+)
+
+;; extends
+(
+	(block_comment) @comment.todo
 	(#match? @comment.todo "(todo|TODO|Todo)")
 	(#set! "priority" 150)
 )
